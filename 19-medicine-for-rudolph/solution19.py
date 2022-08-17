0147a5668b224de819468921d458b06e74e35928
johntelforduk <jtelford1@gmail.com>
2022-08-17 13:34:56 +0100

    Solution to day 19 part 2.

--- 19-medicine-for-rudolph/solution19.py
+++ 19-medicine-for-rudolph/solution19.py
@@ -1,6 +1,9 @@
 # Solution to day 19 of AOC 2015, Medicine for Rudolph
 # https://adventofcode.com/2015/day/19
 
+import re
+from random import shuffle
+
 
 def possible_replacements(r: list, molecule: str) -> list:
     """For parm list of rule tuples (from, to), and parm 'from' molecules, return list of possible replacements."""
@@ -27,13 +30,50 @@ def possible_at_position(s: str, r: list, p: int, l: int) -> set:
         possible.add(replace(s, p, t, l))
     return possible
 
+
+BEST = None
+SHORTEST = None
+
+
+def simplify(m: str, r: list, s: int):
+    """For molecule m, search list of replacements r for simplifications (shorter moecules.
+    Parm s is number of steps taken to produce the parm molecule."""
+    global BEST, SHORTEST
+
+    lm = len(m)
+    if SHORTEST is None:
+        SHORTEST = lm
+    elif lm < SHORTEST:
+        SHORTEST = lm
+        print('SHORTEST, s:', SHORTEST, s)
+
+    if BEST is not None:
+        if s >= BEST:
+            return
+
+    if m == 'e':
+        if BEST is None:
+            BEST = s
+        else:
+            BEST = s
+        print('BEST: ', BEST)
+        return
+
+    shuffle(r)
+    for (f, t) in r:
+        p = m.find(t)
+        if p != -1:
+            new = replace(m, p, f, len(t))
+            simplify(new, r, s + 1)
+
+
 def main():
     f = open('input.txt')
     whole_text = f.read()
     f.close()
 
-    raw_replacements, start = whole_text.split('\n\n')
-    print('start: ', start)
+    raw_replacements, target = whole_text.split('\n\n')
+    print('target: ', target)
     replacements_list = raw_replacements.split('\n')
     replacements = []
     for rule in replacements_list:
@@ -41,17 +81,18 @@ def main():
         replacements.append((f, t))
     print('replacements: ', replacements)
 
+    # --------------- Part 1
     could = set()
-    for p in range(len(start)):
-        print(possible_at_position(start, replacements, p, 1))
-        could = could.union(possible_at_position(start, replacements, p, 1))
-#        could += possible_at_position(start, replacements, p, 1)
+    for p in range(len(target)):
+        could = could.union(possible_at_position(target, replacements, p, 1))
 
-        if p + 1 < len(start):
-            could = could.union(possible_at_position(start, replacements, p, 2))
+        if p + 1 < len(target):
+            could = could.union(possible_at_position(target, replacements, p, 2))
 
-    print('could:', could)
-    print(len(could))
+    print('part 1:', len(could))
+
+    # --------------- Part 2
+    simplify(target, replacements, 0)
 
 
 if __name__ == "__main__":
