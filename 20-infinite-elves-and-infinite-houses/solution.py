d56e2f8ef85f2c269e4db6b173058f1df98d7111
johntelforduk <jtelford1@gmail.com>
2022-08-18 09:45:11 +0100

    Solution to day 20, part 2.

--- 20-infinite-elves-and-infinite-houses/solution.py
+++ 20-infinite-elves-and-infinite-houses/solution.py
@@ -1,32 +1,36 @@
-# Solution to day 8 of AOC 2015, Infinite Elves and Infinite Houses
+# Solution to day 20 of AOC 2015, Infinite Elves and Infinite Houses
 # https://adventofcode.com/2015/day/20
 
-from random import randint
+import math
 
-def factors(x: int) -> list:
-    f = []
-    for t in range(1, 1 + x):
-        if x % t == 0:
-            f.append(t)
-    return f
+def factors(n: int) -> list:
+    small_divisors = [i for i in range(1, int(math.sqrt(n)) + 1) if n % i == 0]
+    large_divisors = [int(n / d) for d in small_divisors if n != d * d]
+    return small_divisors + large_divisors
 
-def presents(x: int) -> int:
+
+def presents1(x: int) -> int:
     p = 0
     for f in factors(x):
         p += 10 * f
     return p
 
+
+def presents2(x: int) -> int:
+    p = 0
+    for f in factors(x):
+        if x <= f * 50:
+            p += 11 * f
+    return p
+
+
 puzzle_input = 34000000
 
 best = None
 
-
-while True:
-    attempt, p = 1, 0
-    while p < puzzle_input:
-        m = randint(1, 15)
-        attempt *= m
-        p = presents(attempt)
-    if best is None or attempt < best:
-        best = attempt
-        print('best, p, puzzle_input:', best, p, puzzle_input)
+attempt = 786240
+while presents2(attempt) < puzzle_input:
+    attempt += 1
+    if attempt % 100000 == 0:
+        print(attempt)
+print(attempt)
